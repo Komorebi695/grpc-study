da1af4544f09ea9b9e569c34c0dc0853564a3d16
niexiaojiang <2570040683@qq.com>
2023-05-25 22:40:16 +0800

    SSL/TLS认证安全传输

--- hello-client/main.go
+++ hello-client/main.go
@@ -4,13 +4,20 @@ import (
 	"context"
 	"fmt"
 	"google.golang.org/grpc"
-	"google.golang.org/grpc/credentials/insecure"
+	"google.golang.org/grpc/credentials"
 	pb "grpcStudy/hello-server/proto"
 	"log"
 )
 
 func main() {
-	coon, err := grpc.Dial("127.0.0.1:9090", grpc.WithTransportCredentials(insecure.NewCredentials()))
+	creds, err := credentials.NewClientTLSFromFile("D:\\Programming\\ProjectCode\\GO\\src\\grpcStudy\\key\\test.pem",
+		"*.xiaojiang.com")
+	if err != nil {
+		return
+	}
+
+	//coon, err := grpc.Dial("127.0.0.1:9090", grpc.WithTransportCredentials(insecure.NewCredentials()))
+	coon, err := grpc.Dial("127.0.0.1:9090", grpc.WithTransportCredentials(creds)) // ssl
 	if err != nil {
 		log.Fatalf("did not connect: %v", err)
 	}
--- hello-server/main.go
+++ hello-server/main.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
 	pb "grpcStudy/hello-server/proto"
 	"net"
 )
@@ -19,6 +20,12 @@ func (s *server) SayHello(ctx context.Context, req *pb.HelloRequest) (*pb.HelloR
 }
 
 func main() {
+	// SSl认证
+	// 参数：certFile, keyFile
+	// 签名证书 和 私钥文件
+	creds, err := credentials.NewServerTLSFromFile("D:\\Programming\\ProjectCode\\GO\\src\\grpcStudy\\key\\test.pem",
+		"D:\\Programming\\ProjectCode\\GO\\src\\grpcStudy\\key\\test.key")
+
 	// 开启端口
 	listen, err := net.Listen("tcp", ":9090")
 	if err != nil {
@@ -26,7 +33,8 @@ func main() {
 		return
 	}
 	// 创建grpc服务
-	grpcServer := grpc.NewServer()
+	//grpcServer := grpc.NewServer()
+	grpcServer := grpc.NewServer(grpc.Creds(creds)) // ssl
 	// 在grpc服务端中去注册自己写的服务
 	pb.RegisterSayHelloServer(grpcServer, &server{})
 	// 启动服务
